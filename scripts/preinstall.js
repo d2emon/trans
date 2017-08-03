412744dbc3c2f4943093b96c04b36c8d61980c1e
Dmitry Kutsenko <d2emonium@gmail.com>
2017-08-03 16:20:59 +0300

    Install script

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,16 +1,7 @@
 var shell = require('shelljs');
-var path = require('path');
 
 console.log("Updating from git");
 shell.exec('git pull');
 
-console.log("Updating pip modules");
-shell.exec("pip install -r requirements.txt");
-shell.exec("flask db upgrade");
-
 console.log("Installing bower");
 shell.exec("bower install");
-
-console.log("Setting up gulp");
-gulpfile = path.join('node_modules', 'gulpfile', 'result', 'gulpfile.js');
-shell.cp(gulpfile, 'gulpfile.js');
